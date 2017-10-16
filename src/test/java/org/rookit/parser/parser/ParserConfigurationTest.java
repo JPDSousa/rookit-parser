2211d5b542e609ad911edbf1f60d86a869798c9d
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-16 10:07:53 +0100

    mocking DBManager

--- src/test/java/org/rookit/parser/parser/ParserConfigurationTest.java
+++ src/test/java/org/rookit/parser/parser/ParserConfigurationTest.java
@@ -22,30 +22,17 @@
 package org.rookit.parser.parser;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.rookit.mongodb.DBManager;
-import org.rookit.mongodb.queries.AlbumQuery;
-import org.rookit.mongodb.queries.ArtistQuery;
-import org.rookit.mongodb.queries.GenreQuery;
-import org.rookit.mongodb.queries.TrackQuery;
-import org.rookit.dm.album.Album;
-import org.rookit.dm.artist.Artist;
-import org.rookit.dm.genre.Genre;
-import org.rookit.dm.parser.IgnoreField;
-import org.rookit.dm.track.Track;
-import org.rookit.parser.parser.Field;
 import org.rookit.parser.parser.Parser;
 import org.rookit.parser.parser.TrackFormat;
 import org.rookit.parser.result.SingleTrackAlbumBuilder;
-import org.smof.gridfs.SmofGridRef;
 
 @SuppressWarnings("javadoc")
 public class ParserConfigurationTest {
@@ -59,7 +46,7 @@ public class ParserConfigurationTest {
 	
 	@Test
 	public final void testDBConnection() {
-		final DBManager db = new DumbDB();
+		final DBManager db = mock(DBManager.class);
 		config.withDBConnection(db);
 		assertEquals(db, config.getDBConnection());
 		//test null db (allowed)
@@ -86,7 +73,7 @@ public class ParserConfigurationTest {
 	public final void testDbStorage() {
 		final boolean store = true;
 		config.withDbStorage(store)
-		.withDBConnection(new DumbDB());
+		.withDBConnection(mock(DBManager.class));
 		assertEquals(store, config.isStoreDB());
 	}
 	
@@ -97,139 +84,6 @@ public class ParserConfigurationTest {
 		assertEquals(formats, config.getFormats());
 		//test null formats (allowed)
 		config.withTrackFormats(null);
-	}
-	
-	private static class DumbDB implements DBManager {
-
-		@Override
-		public void reset() {
-			//intentionally empty
-		}
-
-		@Override
-		public void init() {
-			//intentionally empty
-		}
-
-		@Override
-		public void clear() {
-			//intentionally empty
-		}
-
-		@Override
-		public int getIgnoredOccurrences(String value) {
-			//intentionally empty
-			return 0;
-		}
-
-		@Override
-		public void close() throws IOException {
-			//intentionally empty
-		}
-
-		@Override
-		public void addAlbum(Album arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void addArtist(Artist arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void addGenre(Genre arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void addTrack(Track arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public AlbumQuery getAlbums() {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public ArtistQuery getArtists() {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public GenreQuery getGenres() {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public TrackQuery getTracks() {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public void updateAlbum(Album arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void updateArtist(Artist arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void updateGenre(Genre arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void updateIgnored(IgnoreField arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void updateTrack(Track arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public void loadBucket(String arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public byte[] download(SmofGridRef arg0) {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public InputStream stream(SmofGridRef arg0) {
-			//intentionally empty
-			return null;
-		}
-
-		@Override
-		public int getTrackFormatOccurrences(String arg0) {
-			//intentionally empty
-			return 0;
-		}
-
-		@Override
-		public void updateTrackFormat(org.rookit.dm.parser.TrackFormat arg0) {
-			//intentionally empty
-		}
-
-		@Override
-		public Stream<String> streamTrackFormats() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
 	}
 
 }
