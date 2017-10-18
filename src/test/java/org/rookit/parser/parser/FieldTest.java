c7b63dcfe57b4691796868241b75e51fc24aa51a
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-18 08:34:24 +0100

    FieldTest plan

---
+++ src/test/java/org/rookit/parser/parser/FieldTest.java
@@ -0,0 +1,24 @@
+package org.rookit.parser.parser;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+
+import static org.rookit.parser.parser.Field.*;
+
+@SuppressWarnings("javadoc")
+public class FieldTest {
+
+	@Test
+	public final void test1() {
+	}
+
+}
